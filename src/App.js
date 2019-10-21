e95f2aa6017ef08fd39fd3c2ac89e7ba7cb8d3fa
Klaudia <klaudia.krzywda@gmail.com>
2019-10-21 22:26:39 +0100

    add routing to subpages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import Home from './Containers/Home';
 import Pokedex from './Containers/Pokedex';
+import Abilities from './Containers/Abilities';
+import Items from './Containers/Items';
+import Locations from './Containers/Locations';
+import Moves from './Containers/Moves';
+import Charts from './Containers/Charts';
 import 'semantic-ui-css/semantic.min.css';
 import './style/style.scss';
 
@@ -16,6 +21,12 @@ class App extends React.Component {
                     <Route exact path="/" component={Home} />
                     <Route path="/pokedex" component={Pokedex} />
                     <Route path="/pokedex/:pokemnId" component={Pokedex} />
+                    <Route path="/abilities" component={Abilities} />
+                    <Route path="/items" component={Items} />
+                    <Route path="/locations" component={Locations} />
+                    <Route path="/moves" component={Moves} />
+                    <Route path="/charts" component={Charts} />
+
                 </Router>
             </Container>
         )
---
+++ src/Containers/Moves/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Moves extends Component {
+    render() {
+        return (
+            <div>
+                Moves
+            </div>
+        )
+    }
+}
+
+export default Moves;
